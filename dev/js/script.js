35a7c9f341bcf7f4d603444141dd09f85e61d82d
Dina Hrist <hdina@inbox.ru>
2019-03-20 16:52:07 +0700

    Added hover and slider-brand

--- dev/js/script.js
+++ dev/js/script.js
@@ -21,3 +21,43 @@ $(document).ready(function() {
     });
   }
 });
+
+var slickSlider = $(".js-slider-brand");
+slickSlider.slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  prevArrow:
+    '<div class="slider-brand__arrow-left"><i class="fas fa-angle-left"></i></div>',
+  nextArrow:
+    '<div class="slider-brand__arrow-right"><i class="fas fa-angle-right"></i></div>'
+  /*responsive: [
+            {
+              settings: {
+                slidesToShow: 1,
+                arrows: false,
+              }
+            }
+        ],*/
+});
+
+var slickSlider = $(".js-slider-gallery");
+slickSlider.slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  prevArrow:
+    '<div class="slider-gallery__arrow-left"><i class="fas fa-angle-left"></i></div>',
+  nextArrow:
+    '<div class="slider-gallery__arrow-right"><i class="fas fa-angle-right"></i></div>'
+  /*responsive: [
+            {
+              settings: {
+                slidesToShow: 1,
+                arrows: false,
+              }
+            }
+        ],*/
+});
